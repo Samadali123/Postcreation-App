c0e460039d88c2002070b8b6e51519938ca66c24
Samadali123 <samadali0125@gmail.com>
2024-05-25 19:03:55 +0530

    Add Authentication

--- routes/index.js
+++ routes/index.js
@@ -1,62 +1,102 @@
 var express = require('express');
 var router = express.Router();
 const [users, posts] = require(`./users`);
-const { route } = require('../app');
+
 
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index');
+    try {
+        res.render('index');
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Internal Server Error" })
+    }
 });
 
 
 router.get(`/loginuser`, function(req, res, next) {
-    res.render(`login`)
+    try {
+        res.render(`login`)
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Internal Server Error" })
+    }
 })
 
 
 router.post(`/register`, async function(req, res, next) {
 
-    const User = await users.findOne({ email: req.body.email })
+    try {
+        const User = await users.findOne({ email: req.body.email })
 
-    if (User) {
-        return res.status(401).json({ success: false, message: "User already Registered " })
+        if (User) {
+            return res.status(401).json({ success: false, message: "User already Registered " })
 
-    } else {
-        let createduser = await users.create({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-        })
+        } else {
+            let createduser = await users.create({
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+            })
 
-        res.redirect(`/profile`);
+            res.redirect(`/profile`);
+        }
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Internal Server Error" })
     }
-
-
 
 })
 
+
+router.post("/login", async(req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const User = await users.findOne({ email: email });
+
+        if (User && User.password === password) {
+            res.redirect("/profile")
+        } else {
+            res.status(401).json({ success: false, message: "Email or Password is Incorrect" })
+
+        }
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Internal Server Error" })
+    }
+})
 router.get(`/profile`, async function(req, res, next) {
-    let allposts = await posts.find()
-    res.render(`profile`, { allposts });
+    try {
+        let allposts = await posts.find()
+        res.render(`profile`, { allposts });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Internal Server Error" })
+    }
 })
 
 
 router.get(`/createpost`, function(req, res, next) {
-    res.render(`posts`)
+    try {
+        res.render(`posts`)
+    } catch (error) {
+        res.status(500).json({ success: false, message: "internal Server Error" })
+    }
+
 })
 
 
 
 router.post(`/newpost`, async function(req, res, next) {
-    let newpost = await posts.create({
-        image: req.body.image,
-        caption: req.body.caption,
-    })
+    try {
+        let newpost = await posts.create({
+            image: req.body.image,
+            caption: req.body.caption,
+        })
 
-    // res.send("created ");
-    res.redirect(`/profile`);
+        // res.send("created ");
+        res.redirect(`/profile`);
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Internal Server Error." })
+    }
 })
 
 
